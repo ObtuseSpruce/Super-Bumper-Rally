b25f56d7d199953e1c4012ce09a09196cb3012fd
ObtuseSpruce <williamllapitan@gmail.com>
2020-04-06 12:06:42 -0700

    removed debug

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let config = {
     physics: {
         default: 'arcade',
         arcade: {
-            debug: true
+            debug: false,
         }
     },
     scene: [Scene2, Scene1, Scene3]
